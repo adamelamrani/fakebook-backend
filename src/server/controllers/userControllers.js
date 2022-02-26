bda5e26b7986ee80f0ff4c56a11ab6f04c3e8ed9
adam-elamrani <adelamco1091@outlook.com>
2022-02-26 10:13:48 +0100

    Tested userControllers login

--- src/server/controllers/userControllers.js
+++ src/server/controllers/userControllers.js
@@ -8,28 +8,17 @@ const User = require("../../db/models/User");
 const login = async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
-  try {
-    if (!username) {
-      const error = new Error("Something went wrong");
-      debug(chalk.red(`Username not found: ${error}`));
-      res.status(404).json(error);
-    } else {
-      const passwordComparison = await bcrypt.compare(password, user.password);
+  const passwordComparison = await bcrypt.compare(password, user.password);
 
-      if (!passwordComparison) {
-        const error = new Error("Something went wrong");
-        debug(chalk.red(`Wrong password: ${error}`));
-        res.status(400).json(error.message);
-        next(error);
-      } else {
-        const data = { username };
-        const token = jwt.sign(data, process.env.SECRET_KEY);
-        res.json({ token });
-      }
-    }
-  } catch (error) {
-    debug(chalk.bgRedBright.yellowBright(`Error: ${error.message}`));
+  if (!user || !passwordComparison) {
+    const error = new Error("Something went wrong");
+    debug(chalk.red(`Username not found: ${error}`));
+    res.status(404).json({ error: error.message });
     next(error);
+  } else {
+    const data = { username };
+    const token = jwt.sign(data, process.env.SECRET_KEY);
+    res.json({ token });
   }
 };
 
