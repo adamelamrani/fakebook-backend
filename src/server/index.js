b7cbe0cff0e6e5664c80f96d746e17eb76ddab51
adam-elamrani <adelamco1091@outlook.com>
2022-02-26 22:39:49 +0100

    Npm installed cors and added it to index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const router = require("./routes/usersRouters");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/user", router);
