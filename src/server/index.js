8ffcab8704c8f3f466a4780d28d31bcb34ff39d2
adam-elamrani <adelamco1091@outlook.com>
2022-02-27 08:28:25 +0100

    corrected cors

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,8 @@ const router = require("./routes/usersRouters");
 
 const app = express();
 
-app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 app.use("/user", router);
 module.exports = app;
