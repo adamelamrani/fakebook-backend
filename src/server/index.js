567edddfd24510de6b791406e1610bc6852cd293
adam-elamrani <adelamco1091@outlook.com>
2022-02-27 08:46:59 +0100

    Added cors options

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use("/user", router);
 module.exports = app;
